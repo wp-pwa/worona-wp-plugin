bdda2d76a677c5eeb507eb4356f6d95d1c106972
LuisHerranz <luisherranz@gmail.com>
2014-06-10 12:46:01 +0200

    First commit

---
+++ worona.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Worona
+Plugin URI: http://www.worona.org/
+Description: Turn your WordPress site into a native iOS, Android and Windows Phone App.
+Version: 1.0.0
+Author: Benuit
+Author URI: http://www.benuit.com/
+License: GPL v3
+Copyright: Benuit
+*/
