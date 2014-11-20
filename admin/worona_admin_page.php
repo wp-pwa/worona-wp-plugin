cad9aca0f9c136d602c62bcbb5add41afa7e2f46
Pablo Postigo Delgado <pablo@benuit.com>
2014-11-20 10:45:10 +0100

    Adding "Publish" & "Extensions" messages to the plugin page.

--- admin/worona_admin_page.php
+++ admin/worona_admin_page.php
@@ -2,8 +2,25 @@
 	<h1><img src="<?php echo trailingslashit(plugin_dir_url( "worona-logo.png" )) . "worona/assets/worona-logo.png"?>"></h1>
 	<br>
 	<div class="updated worona-message"> 
-		<p>Please follow the <strong>Worona Start Tutorial</strong> to get your app ready!<br>
-		<a class="wr-button" href="https://www.worona.org/start" target="_blank">Start Now!</a></p>
+		<h1>Mobile App</h1>
+		<p class="wr-text">Congratulations you have installed <strong>Worona WP Plugin!</strong></p>
+		<p class="wr-text">There are some steps left to have your Mobile App ready:</p>
+		<p class="wr-text">
+		 	<a class="wr-button" href="https://www.worona.org/start" target="_blank">Start Now!</a>
+		</p>
 	</div>
 	<br>
+	<div class="updated worona-message"> 
+		<h1>Extensions</h1>
+		<p class="wr-text">To give <strong>extra powers</strong> to your Mobile App visit our <a href="https://www.worona.org/extensions" target="_blank"> Extensions marketplace</a>.</p>
+	</div>
+	<br>
+	<div class="updated worona-message"> 
+		<h1>Publish</h1>
+		<p class="wr-text">If you want, we can upload your App to the stores: <a href="https://www.worona.org/publish" target="_blank">Worona Publish Service</a>.</p>
+	</div>
+	<br>
+	<p class="wr-text small">
+		<input type="checkbox" checked id="wr-plugin-stats"> Help us improve Worona with some <strong>anonymous</strong> usage stats! Thanks!
+	</p>
 </div>
